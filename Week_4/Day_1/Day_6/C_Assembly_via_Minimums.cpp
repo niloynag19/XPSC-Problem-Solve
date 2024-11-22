75337f773cf7d1e9ed015a4bd0c9f5efaf238de7
niloynag19 <niloynag19@gmail.com>
2024-11-22 23:58:40 +0600

    Update C_Assembly_via_Minimums.cpp

--- Week_4/Day_1/Day_6/C_Assembly_via_Minimums.cpp
+++ Week_4/Day_1/Day_6/C_Assembly_via_Minimums.cpp
@@ -1,48 +1,37 @@
-#include <bits/stdc++.h>
-
-#define br "\n"
+#include<bits/stdc++.h>
 #define ll long long
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
 using namespace std;
-
 int main()
 {
-    // _Created :  23 April 2024 ||   17:30:48
-    // _File    :  C_Assembly_via_Minimums.cpp
-    // Writer    :  Md. Nazmus Sakib
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    // اللهم صل على سيدنا محمدن النبى الامى وآله وسلم
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    ll t;
-    cin >> t;
-    while (t--)
+    int t;
+    cin>>t;
+    while(t--)
     {
         ll n;
-        cin >> n;
-        ll m = (n * (n - 1)) / 2;
-        vector<ll> a(n);
-        vector<ll> b(m);
-        for (ll i = 0; i < m; i++)
+        cin>>n;
+        ll m=(n*(n-1))/2;
+        vector<ll>a(n),b(m);
+        for(int i=0;i<m;i++)
         {
-            cin >> b[i];
+            cin>>b[i];
         }
-        sort(b.begin(), b.end());
-        ll x = 1, index = 0;
-        ll from_B_to_A = n - x;
-        while (n - x > 0)
+        sort(b.begin(),b.end());
+        ll x=1,idx=0;
+        ll cnt=n-x;
+        while(n-x>0)
         {
-            a[index++] = b[from_B_to_A - 1];
+            a[idx++]=b[cnt-1];
             x++;
-            from_B_to_A += n - x;
+            cnt+=n-x;
         }
-        a[n - 1] = 1e9;
-        for (ll aa : a)
+        a[n-1]=1e9;
+        for(ll it:a)
         {
-            cout << aa << " ";
+            cout<<it<<" ";
         }
-        cout << br;
+        cout<<endl;
     }
     return 0;
 }
