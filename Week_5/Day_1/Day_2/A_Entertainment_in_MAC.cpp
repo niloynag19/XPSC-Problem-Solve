330244a14bea4c5411ce5b6244660e10227b7f23
niloynag19 <niloynag19@gmail.com>
2024-11-25 16:45:11 +0600

    Add files via upload
    add week_5 day_2 code

---
+++ Week_5/Day_1/Day_2/A_Entertainment_in_MAC.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        string rev=s;
+        reverse(rev.begin(),rev.end());
+        if(s<=rev)
+        {
+            cout<<s<<endl;
+        }
+        else 
+        {
+            cout<<rev<<s<<endl;
+        }
+    }
+    return 0;
+}
